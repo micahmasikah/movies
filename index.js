f54b2d2c9e32ad4510bd624e987d8b433bc7161b
Micah Masikah <mmasikah7@gmail.com>
2023-01-06 06:58:28 +0300

    Training on Tables

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form');
+/*const form = document.getElementById('form');
 form.addEventListener('submit', (evnt) => {
     evnt.preventDefault();
     const firstname = document.getElementById('firstname').value.toUpperCase();
@@ -7,4 +7,20 @@ form.addEventListener('submit', (evnt) => {
     const age = document.getElementById('age').value;
 
     console.table({ firstname, lastname, email, age });
-});
+});*/
+// Variables, types of variables, loops, conditions, collections, objects, functions
+
+// let, const
+
+function isEven(num) {
+    if (num === 0) {
+        return true;
+    }
+    return (num % 2) === 0;
+}
+
+console.log(`0 is even = `, isEven(0));
+console.log(`3 is even = `, isEven(3));
+console.log(`-4 is even = `, isEven(-4));
+console.log(`18 is even = `, isEven(18));
+console.log(`10145570 is even = `, isEven(10145570));
